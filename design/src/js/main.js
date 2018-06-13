821684d5334a7b8420f916296214a311863b4129
Matheus Luiz da Silva <matheus@bonsaiux.com.br>
2018-06-13 16:37:30 -0300

    Contact form, ajax and recaptcha

--- design/src/js/main.js
+++ design/src/js/main.js
@@ -1,9 +1,9 @@
-$(function(){
+$(document).ready(function(){
   var navItems = $('.nav-item')
 
   $(window).bind('scroll', function () {
     var scrollTop = $(window).scrollTop()
-    console.log(scrollTop)
+    // console.log(scrollTop)
     switch (true) {
       case scrollTop < 50:
         $('.navbar').removeClass('navbar--fixed')
@@ -43,4 +43,91 @@ $(function(){
         break
     }
   });
+
+  $(".contact__form").on("submit", function(event) {
+    event.preventDefault();
+    
+    var form = $(this);
+    var inputs = form.find("input, textarea");
+    var sitekey = "6LdFyl4UAAAAALq8_mnxZDyc4SbCj0PQKT9bHI77";
+    var size = "invisible";
+    var invalid = false;
+
+    if (invalid) {
+      return false;
+    } else {
+      grecaptcha.render("recaptcha", {
+        'sitekey': sitekey,
+        'size': size,
+        'callback': function(token) {
+          var btn = form.find("button[type='submit']");
+          var method = "POST";
+          var url = "http://localhost:8080/contact/send_mail.php";
+          var data = {
+            g_recaptcha_response: token
+          };
+
+          inputs.each(function(index, element) {
+            var input = element;
+            data[input.name] = input.value;
+          });
+
+          $.ajax({
+            url: url,
+            method: method,
+            data: data,
+            beforeSend: function () {
+              btn.text("Enviando...");
+            }
+          }).done(function (res) {
+            console.log(res);
+            // form.reset();
+
+            btn.text("Enviado!");
+
+            setTimeout(function() {
+              $(resContainer).slideUp("slow");
+              button.textContent = "Enviar";
+            }, 5000);
+          }).fail(function (xhr) {
+            console.log(xhr);
+          });
+        }
+      });
+
+      grecaptcha.execute();
+    }
+  });
 });
+
+function inputValidation (inputs) {
+  var invalid = false;
+
+  inputs.each(function (index, element) {
+    var input = element;
+    var group = input.parent(".form-group");
+    var textHelp = group.find(".text-help");
+
+    if (input.val() == "") {
+      input.addClass("invalid");
+      invalid = true;
+    } else {
+      if (input.attr("type") == "email") {
+        var regex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+        
+        if (!regex.test(input.val())) {
+          invalid = true;
+          textHelp.text("Forneça um endereço de email válido.");
+          input.addClass("invalid");
+        } else {
+          input.removeClass("invalid");
+          textHelp.text("Preencha este campo.");
+        }
+      } else {
+        input.removeClass("invalid");
+      }
+    }
+
+    return invalid;
+  });
+}
